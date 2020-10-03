5d6f085ff6b06fe82db0a5fb60e3d9d7190c006a
Andrei Neagoie <aneagoie@MacBook-Pro.local>
2020-10-03 11:22:48 -0400

    updated libraries and readme

--- components/image.js
+++
@@ -1,9 +0,0 @@
-const Image = () => {
-  return (
-    <div>
-      <img src='http://www.airmadagascar.com/sites/all/themes/Porto_sub/img/illus-cameleon.png'/>
-    </div>
-  );
-}
-
-export default Image;
